e094570e1204ccdcb519421e1927dc123da9a247
Jedi <12877446+jedielson@users.noreply.github.com>
2026-01-12 18:13:13 +0000

    chore: creating project

---
+++ src/main/java/org/churchcrm/churchcrmapi/ChurchCrmApiApplication.java
@@ -0,0 +1,13 @@
+package org.churchcrm.churchcrmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchCrmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchCrmApiApplication.class, args);
+    }
+
+}
